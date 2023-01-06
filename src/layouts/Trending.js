d6e57109e08f422578db8b2fa3dcdf228e1af278
Guilherme Morais <guilherme.morais0111@gmail.com>
2023-01-06 15:04:25 -0300

    feat: create the trending box

---
+++ src/layouts/Trending.js
@@ -0,0 +1,70 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Trending({trending}) {
+    const renderHashtags = [];
+    const navigate = useNavigate();
+
+    if(trending.length===0) {
+        renderHashtags.push(<ZeroTrendings key={0}>Ainda não há hashtag a ser mostrada</ZeroTrendings>);
+    } else {
+        trending.map((e,i) => renderHashtags.push(<Hashtag key={i} onClick={() => navigate(`/hashtag/${e.name}`)}># {e.name}</Hashtag>));
+    }
+
+    return(
+    <Container>
+        <TopDiv>trending</TopDiv>
+        <Topics>
+            {renderHashtags}
+        </Topics>
+    </Container>
+    );
+}
+
+const Container = styled.div`
+  width: 300px;
+  margin-top: 40px;
+`;
+
+const TopDiv = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #171717;
+    border-radius: 15px 15px 0px 0px;
+    border-bottom: 1px solid #333333;
+    display: flex;
+    padding: 15px 15px;
+    font-family: "Oswald";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 27px;
+    color: #ffffff;
+`;
+
+const Topics = styled.div`
+    width: 100%;
+    max-height: 345px;
+    background-color: #171717;
+    border-radius: 0px 0px 15px 15px;
+    border-top: 1px solid #333333;
+    padding: 30px 15px;
+`;
+
+const Hashtag = styled.p`
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    color: #ffffff;
+    margin: 10px 0px;
+    cursor: pointer;
+`;
+
+const ZeroTrendings = styled.p`
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    color: #ffffff;
+    margin: 10px 0px;
+`
--- src/pages/Timeline/Timeline.js
+++ src/pages/Timeline/Timeline.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from "react";
+import { getTrending } from "../../api/trending";
 import getPosts from "../../components/Post/getPosts";
 import Post from "../../components/Post/Post";
 import MainLayout from "../../layouts/MainLayout/MainLayout";
+import Trending from "../../layouts/Trending";
 import CreatePost from "./CreatePost";
-import { Container, Loading, PageTitle, PostList } from "./styles";
+import { Container, LeftContainer, Loading, PageTitle, PostList, SubContainer } from "./styles";
 
 export default function Timeline() {
   const [posts, setPosts] = useState([]);
-  useEffect(() => getPosts(setPosts), []);
+  const [trending, setTrending] = useState([]);
+  useEffect(() => {getPosts(setPosts); getTrending(setTrending)}, []);
 
   return (
     <MainLayout>
       <Container>
         <PageTitle>timeline</PageTitle>
-        <CreatePost setPosts={setPosts}/>
+        <SubContainer>
+          <LeftContainer>
+        <CreatePost setPosts={setPosts} setTrending={setTrending} />
         <PostList>
           {posts.length === 0 ? (
             <Loading src="https://yorkdalelincoln.com/wp-content/themes/lbx-iag/resources/images/spinner.gif" />
@@ -21,6 +26,9 @@ export default function Timeline() {
             posts.map((post) => <Post key={post.id} data={post} />)
           )}
         </PostList>
+        </LeftContainer>
+        <Trending trending={trending}/>
+        </SubContainer>
       </Container>
     </MainLayout>
   );
